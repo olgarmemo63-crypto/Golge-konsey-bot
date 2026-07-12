fd7f2d5b74a6d6e190ee91149a1df8a657d0ddac
olgarmemo63-crypto <olgarmemo63@gmail.com>
2026-07-12 09:06:52 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,19 +1,26 @@
 import os
-import google.generativeai as genai
+from google import genai
 from telegram import Update
-from telegram.ext import Application, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-genai.configure(api_key=GEMINI_API_KEY)
-
-model = genai.GenerativeModel("gemini-1.5-flash")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 async def cevap_ver(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    mesaj = update.message.text
-    cevap = model.generate_content(mesaj)
-    await update.message.reply_text(cevap.text)
+    try:
+        mesaj = update.message.text
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=mesaj
+        )
+
+        await update.message.reply_text(response.text)
+
+    except Exception as e:
+        await update.message.reply_text(f"Hata oluştu:\n{e}")
 
 app = Application.builder().token(TELEGRAM_TOKEN).build()
 
@@ -21,6 +28,6 @@ app.add_handler(
     MessageHandler(filters.TEXT & ~filters.COMMAND, cevap_ver)
 )
 
-prinprint("Bot aktif ve polling başladı!")
+print("🤖 Gölge Konsey Bot aktif!")
 
 app.run_polling()
